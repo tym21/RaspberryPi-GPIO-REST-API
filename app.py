35e7286998996a8bcc6c7b147439eaf4a202f2ee
tym21 <56368000+timknothe@users.noreply.github.com>
2021-02-09 17:53:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource, reqparse, abort
+
+gpio_put_args = reqparse.RequestParser()
+gpio_put_args.add_argument("status", type=int, help="Status of the GPIO is required", required=True)
+
+
+def check_gpio(pin):
+    if pin > 26:
+        abort(404, message="Pin is not valid")
+
+
+def check_status(status):
+    if not status == 0 or status == 1:
+        abort(404, message="Status is not valid")
+
+
+class GPIOControl(Resource):
+    def get(self, gpio):
+        check_gpio(gpio)
+        status = 1
+        return {"gpio": gpio,
+                "status": status
+                }
+
+    def put(self, pin):
+        check_gpio(pin)
+        args = gpio_put_args.parse_args()
+        check_status(args["status"])
+        return {"gpio": pin,
+                "status": args["status"]
+                }
 
 
 def create_app():
@@ -7,14 +37,17 @@ def create_app():
     flask_app = Flask(__name__)
     # load the config from external file
     flask_app.config.from_pyfile("config.cfg")
-
-    @flask_app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     return flask_app
 
 
+def create_api(app):
+    api = Api(app)
+    return api
+
+
+app = create_app()
+api = create_api(app)
+api.add_resource(GPIOControl, '/gpio/<int:pin>/status')
+
 if __name__ == '__main__':
-    app = create_app()
     app.run()
